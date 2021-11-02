8f8e33ba278131ea666c39e75329152074b347b0
Elena Shustova <alenyf-ka@mail.ru>
2021-11-02 11:12:22 +0300

    feat: add solution 1 task

---
+++ 01-read-file/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const namePath = path.join(__dirname, 'text.txt')
+const readableStream = fs.createReadStream(namePath, 'utf-8');
+
+let data = '';
+
+readableStream.on('data', chunk => {
+    chunk = chunk.trim();
+        data += chunk;
+});
+readableStream.on('end', () => console.log(data));
+readableStream.on('error', error => console.log('Error', error.message));
