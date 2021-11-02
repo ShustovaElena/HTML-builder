c601f1040a737aff08c35eb5801ce3c9e3c4df3e
Elena Shustova <alenyf-ka@mail.ru>
2021-11-02 21:54:33 +0300

    feat: add solution 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 
+const namePathCopy = path.join(__dirname,'files-copy');
+const namePath = path.join(__dirname,'files');
+  
+function copyDir() {
+const fsPromises = fs.promises;
+fsPromises.mkdir(namePathCopy).then(function() {
+    console.log('Directory created successfully');
+}).catch(function() {
+    console.log('Directory already exists');
+});
+
+fs.readdir(namePath, 'utf-8', (err, files) => {
+    if (err) throw err;
+
+    for (let i = 0; i < files.length; i++) {
+        try {
+            fsPromises.copyFile(path.join(namePath,files[i]), path.join(namePathCopy,files[i]));
+        } catch {
+            console.log('The file could not be copied');
+        }
+    }
+
+    fs.readdir(namePathCopy, 'utf-8', (err, elements) => {
+        if (err) throw err;
+        let deleteFile = diff(elements, files);
+        if (deleteFile.length != 0) {   
+            let deleteFilePath = path.join(namePathCopy, deleteFile[0]);
+            fsPromises.unlink(deleteFilePath);
+        }
+    });
+
+
+});
+
+}
+copyDir();
+
+function diff(a, b) {
+    return a.filter(function(i) {return b.indexOf(i) < 0;});
+};
